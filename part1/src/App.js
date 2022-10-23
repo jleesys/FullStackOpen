272e20a390fb44248df7f0179ef0af2618791659
jleesys949 <jlee.post24@gmail.com>
2022-10-23 14:26:29 -0700

    Complete ex1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -30,7 +30,7 @@ const Total = (props) => {
   let sum = 0;
   const arrayOfParts = props.total;
   arrayOfParts.forEach(value => sum += value.exercises);
-    
+
   return (
     <p>
       Number of exercises {sum}
@@ -39,28 +39,30 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-    name: 'Fundamentals of React',
-    exercises: 10
-    },
-    {
-    name: 'Using props to pass data',
-    exercises: 7
-    },
-    {
-    name: 'State of a component',
-    exercises: 14
-    }
-  ]
 
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
-      <Header courseName={course}/>
-      <Content contents={parts} />
-      <Total total={parts} />
+      <Header courseName={course.name} />
+      <Content contents={course.parts} />
+      <Total total={course.parts} />
     </>
   )
 }
