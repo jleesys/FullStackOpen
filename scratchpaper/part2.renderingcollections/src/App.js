b89df1c1474bd37cbbf75043023084b7adbd9994
jleesys949 <jlee.post24@gmail.com>
2022-10-31 18:21:44 -0700

    Destructured notes obj

--- scratchpaper/part2.renderingcollections/src/App.js
+++ scratchpaper/part2.renderingcollections/src/App.js
@@ -1,12 +1,19 @@
-const App = (props) => {
-  const { notes } = props
-  window.notes = notes;
+const Note = (props) => {
+  const {content} = props;
+  return (
+    <li>{content}</li>
+  )
+}
+
+const App = ({notes}) => {
+  // const { notes } = props
+  // window.notes = notes;
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map((notes) => <li key={notes.id}>{notes.content}</li>)}
+        {notes.map((notes) => <Note content={notes.content} key={notes.id}/> )}
       </ul>
     </div>
   )
