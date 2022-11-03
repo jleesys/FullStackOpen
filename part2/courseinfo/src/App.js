2cf2e9951cdd61b0bbfc19ae1d52e3d694a0c621
jleesys949 <jlee.post24@gmail.com>
2022-11-02 20:08:55 -0700

    Implement Courses component
    Courses component takes arbitrary # of Course (individual)
    components

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,5 +1,14 @@
 import { useState } from 'react'
 
+const Courses = ({courses}) => {
+  return (
+    <>
+      {courses.map((courseObj) => 
+        <Course course={courseObj} />)} 
+    </>
+  )
+}
+
 const Course = ({ course }) => {
   const { parts, id, name } = course;
 
@@ -11,13 +20,6 @@ const Course = ({ course }) => {
     return sumOfParts;
   }
 
-  /*
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i].exercises;
-  }
-  return sum;
-  */
 
   return (
     <>
@@ -29,29 +31,57 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
 
-  return <Course course={course} />
+
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  // return <Course course={course} />
+
+  return (
+    <Courses courses={courses} />
+  )
 }
 
 export default App
