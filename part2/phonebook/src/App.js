4838aab92625d9e911512f7a776ef7ed35c07f04
jleesys949 <jlee.post24@gmail.com>
2022-11-05 15:38:53 -0700

    Add event to submit numbers and names

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,15 @@ import { useState } from 'react';
 
 const App = () => {
 
+  // State vars for NAME entry
   const [names, setNames] = useState([]);
   const [newName, setNewName] = useState('Enter new name here');
+  // State vars for NUMBER entry
+  const [numbers, setNumbers] = useState([]);
+  const [newNumber, setNewNumber] = useState('Enter new number here');
 
   window.names = names;
+  window.numbers = numbers;
 
   // ---------------- NAME CHANGES ----------------
   // whenever a change in the name form is detected
@@ -15,6 +20,28 @@ const App = () => {
     console.log('logging new name:', event.target.value);
   }
 
+  // const handleNameSubmission = (event) => {
+  //   event.preventDefault();
+  //   if (names.includes(newName)) {
+  //     alert(`${newName} is already present in phonebook.`);
+  //     return;
+  //   }
+  //   console.log('adding name to list: ', newName)
+  //   setNames(names.concat(newName));
+  // }
+
+  // ---------------- NUMBER CHANGES ----------------
+  const handleNumberFormChange = (event) => {
+    setNewNumber(event.target.value);
+  }
+
+  /*
+  const handleNumberSubmission = (event) => {
+    event.preventDefault();
+    setNumbers(numbers.concat(newNumber));
+  }
+  */
+
   const handleNameSubmission = (event) => {
     event.preventDefault();
     if (names.includes(newName)) {
@@ -23,13 +50,7 @@ const App = () => {
     }
     console.log('adding name to list: ', newName)
     setNames(names.concat(newName));
-  }
-
-  // ---------------- NUMBER CHANGES ----------------
-  const handleNumberFormChange = (event) => {
-  }
-
-  const handleNumberSubmission = (event) => {
+    setNumbers(numbers.concat(newNumber));
   }
 
   return (
@@ -38,6 +59,9 @@ const App = () => {
       <form onSubmit={handleNameSubmission}>
         <div>
           Name: <input value={newName} onChange={handleNameFormChange} />
+        </div>
+        <div>
+          Number: <input value={newNumber} onChange={handleNumberFormChange} />
         </div>
         <button type="submit">add</button>
       </form>
