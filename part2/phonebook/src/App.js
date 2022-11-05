c0fecae5cf738d771a27aa2368391116f9ebe768
jleesys949 <jlee.post24@gmail.com>
2022-11-05 14:19:37 -0700

    Create eventhandler func for form change

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,25 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 const App = () => {
 
   const [numbers, setNumbers] = useState([]);
+  const [newName, setNewName] = useState('Enter new name here');
+
+  // whenever a change in the name form is detected
+  // set the new name up to be added to list 'numbers'
+  const handleFormChange = (event) => {
+    setNewName(event.target.value);
+    console.log('logging new name:', event.target.value);
+  }
 
   return (
     <div>
       <h1>Phonebook</h1>
-      <form>
+      <form /* onSubmit */>
         <div>
-        Name: <input />
+          Name: <input value={newName} onChange={handleFormChange}/>
         </div>
-        <div>
-          <button>add</button>
-        </div>
+        <button>add</button>
       </form>
       <h1>Numbers</h1>
     </div>
