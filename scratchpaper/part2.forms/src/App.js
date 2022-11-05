0eab8fd71026c456eca28e27899f72cea2b2289b
jleesys949 <jlee.post24@gmail.com>
2022-11-04 20:03:02 -0700

    Add event

--- scratchpaper/part2.forms/src/App.js
+++ scratchpaper/part2.forms/src/App.js
@@ -2,16 +2,19 @@ import Note from './components/Note';
 
 const App = ({notes}) => {
 
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log(notes, event.target);
+  }
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map((notes) => <Note content={notes.content} key={notes.id}/> )}
       </ul>
-      <form>
-        <input>
-        </input>
-        <button type="submit">save</button>
+      <form onSubmit={addNote}>
+        <input/>
+        <button type="submit" >save</button>
       </form>
     </div>
   )
