7bcfb2be5a5f9a28e743ca258dd6c5a121b410b8
jleesys949 <jlee.post24@gmail.com>
2022-11-04 20:45:17 -0700

    Add and implement event handlers for add new note

--- scratchpaper/part2.forms/src/App.js
+++ scratchpaper/part2.forms/src/App.js
@@ -4,11 +4,28 @@ import {useState} from 'react';
 const App = (props) => {
 
   const [notes,setNotes] = useState(props.notes);
+  const [newNote,setNewNote] = useState('this is my new note.');
 
   const addNote = (event) => {
     event.preventDefault();
     console.log('button clicked', event.target);
+    const newObject = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date(),
+      important: Math.random() < .5
+    }
+    console.log(newObject)
+    const newNotesObj = notes.concat(newObject);
+    setNotes(newNotesObj);
+    setNewNote('');
   }
+
+  const handleNoteChange = (event) => {
+    console.log('setting new note value, ',event.target.value);
+    setNewNote(event.target.value);
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -16,7 +33,7 @@ const App = (props) => {
         {notes.map((notes) => <Note content={notes.content} key={notes.id}/> )}
       </ul>
       <form onSubmit={addNote}>
-        <input/>
+        <input value={newNote} onChange={handleNoteChange}/>
         <button type="submit" >save</button>
       </form>
     </div>
