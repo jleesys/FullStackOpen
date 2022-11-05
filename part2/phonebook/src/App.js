606f39e417c23ae2293ab8c150572f0b6cbd09b7
jleesys949 <jlee.post24@gmail.com>
2022-11-05 13:59:09 -0700

    Create form

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,21 @@
+import {useState} from 'react';
 
 const App = () => {
+
+  const [numbers, setNumbers] = useState([]);
+
   return (
-  <h1>react dom</h1>
+    <div>
+      <h1>Phonebook</h1>
+      <form>
+        <>name: </>
+        <input />
+        <div>
+          <button>add</button>
+        </div>
+      </form>
+      <h1>Numbers</h1>
+    </div>
   );
 }
 
