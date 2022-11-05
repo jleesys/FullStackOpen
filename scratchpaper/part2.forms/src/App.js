8dc5f81028df0521683316dc26df2cfc646c2084
jleesys949 <jlee.post24@gmail.com>
2022-11-04 18:59:52 -0700

    Create project
    Duplicate of Notes project from earlier in part 2

---
+++ scratchpaper/part2.forms/src/App.js
@@ -0,0 +1,17 @@
+import Note from './components/Note';
+
+const App = ({notes}) => {
+  // const { notes } = props
+  // window.notes = notes;
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((notes) => <Note content={notes.content} key={notes.id}/> )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
