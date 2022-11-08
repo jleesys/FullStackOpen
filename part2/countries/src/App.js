6a0e795b9e22fa783344d1c1ae7ab2509d566064
jleesys949 <jlee.post24@gmail.com>
2022-11-07 17:47:17 -0800

    Implement search

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,20 +1,53 @@
 
+// should gather data from endpoint https://restcountries.com/v3.1/all
+
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [searchField, setSearchField] = useState('');
+  const [countriesToShow, setCountriesToShow] = useState([]);
+
+  window.countries = countries;
+
+  const handleSearchChange = (event) => {
+    setSearchField(event.target.value);
+    const arrayResults = findCountry(searchField);
+    console.log(arrayResults);
+  }
+
+  const findCountry = (searchTerm) => {
+    const search = searchTerm.toLowerCase();
+    const arrayOfResults = [];
+    console.log('executing search for', search);
+    for (let i = 0; i < countries.length; i++) {
+      if (countries[i].name.common.toLowerCase().includes(search.toLowerCase()) || countries[i].name.official.toLowerCase().includes(search.toLowerCase())) {
+        console.log(`found country ${countries[i].name.official}`);
+        arrayOfResults.push(countries[i]);
+      }
+    }
+    return arrayOfResults;
+  }
+
+  useEffect(() => {
+    // console.log('promise created');
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then((response) => {
+        // console.log('promise fulfilled', response.data);
+        setCountries(response.data);
+      });
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Find Countries
+      <form>
+        <input placeholder="country" autoFocus onChange={handleSearchChange}></input>
+      </form>
+
     </div>
   );
 }
