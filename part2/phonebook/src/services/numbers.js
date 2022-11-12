907b64ec1f64820caa75b4b294644af97dbd977d
jleesys949 <jlee.post24@gmail.com>
2022-11-11 18:58:28 -0800

    Fix indent
    Revert db

--- part2/phonebook/src/services/numbers.js
+++ part2/phonebook/src/services/numbers.js
@@ -2,9 +2,10 @@ import axios from 'axios'
 const baseURL = 'http://localhost:3001/persons';
 
 const getAll = () => {
-    const request = axios
-        .get(baseURL)
-        .then(response => response.data);
+    const request =
+        axios
+            .get(baseURL)
+            .then(response => response.data);
     return request;
 }
 
