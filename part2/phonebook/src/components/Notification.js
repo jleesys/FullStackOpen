29743f0bb31f7f9de46eb205d12500ec0b852281
jleesys949 <jlee.post24@gmail.com>
2022-11-12 15:14:58 -0800

    Add notif style

--- part2/phonebook/src/components/Notification.js
+++ part2/phonebook/src/components/Notification.js
@@ -1,7 +1,27 @@
-const Notification = ({message}) => {
-    
+const Notification = ({ message }) => {
+    if (message?.includes('Error')) {
+        var style = {
+            color: 'red',
+            fontStyle: 'italic',
+            fontSize: '16',
+            padding: '25',
+            outline: '5',
+            backgroundColor: 'grey'
+        }
+    } else {
+
+        var style = {
+            color: 'green',
+            fontStyle: 'italic',
+            fontSize: '16',
+            padding: '25',
+            outline: '5',
+            backgroundColor: 'grey'
+        }
+    }
+
     return (
-        <div>
+        <div style={style}>
             {message}
         </div>
     )
