f9de1c9c5ba43d5d1f1d60af70eef609226af140
jleesys949 <jlee.post24@gmail.com>
2022-11-13 14:36:57 -0800

    Add POST for new note

--- scratchpaper/part3.expresscreation/index.js
+++ scratchpaper/part3.expresscreation/index.js
@@ -2,6 +2,7 @@ const repl = require('repl');
 
 const express = require('express')
 const app = express()
+app.use(express.json());
 
 let notes = [
     {
@@ -49,6 +50,19 @@ app.delete('/api/notes/:id', (request, response) => {
     notes = notes.filter(note => note.id !== id);
 
     response.status(204).end();
+})
+
+// add note
+app.post('/api/notes', (request, response) => {
+    const currentID = notes.length > 0 ? Math
+        .max(...notes.map(note => note.id)) + 1 : 0;
+    const noteToAdd = request.body;
+    console.log(noteToAdd, request.get('Content-Type'));
+    noteToAdd.id = currentID;
+
+    notes = notes.concat(noteToAdd);
+
+    response.json(noteToAdd);
 })
 
 const PORT = 3001
