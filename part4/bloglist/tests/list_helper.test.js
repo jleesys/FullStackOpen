6a9128a4b208b06de7066637da75b8de1cac542a
jleesys949 <jlee.post24@gmail.com>
2022-11-24 21:50:29 -0800

    Fave blog test

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -21,3 +21,20 @@ describe('total likes', () => {
         expect(result).toBe(36);
     })
 })
+
+describe('favorite blog', () => {
+    test('is correctly chosen', () => {
+        const favoriteBlog = listHelper.favoriteBlog(listBlogs);
+        const correctBlog = {
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        };
+        expect(favoriteBlog).toEqual(correctBlog);
+    }
+    )
+}
+)
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,9 @@
+// test func for testing purpose
 const dummy = (blogs) => {
     return 1;
 }
 
+// gleans the total num likes from an array of blogs
 const totalLikes = (blogs) => {
     const total = blogs.reduce((runningTotal, blog) => {
         return runningTotal += blog.likes;
@@ -9,4 +11,19 @@ const totalLikes = (blogs) => {
     return total;
 }
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+    // let faveBlog = blogs[0];
+    // for (let i = 0; i < blogs.length; i++) {
+    //     if (blogs[i].likes > faveBlog.likes) {
+    //         faveBlog = blogs[i];
+    //     }
+    // }
+
+    const faveBlog = blogs.reduce((faveBlog, currBlog) => {
+        if (currBlog.likes > faveBlog.likes) return currBlog;
+        else return faveBlog;
+    })
+    return faveBlog;
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog };
