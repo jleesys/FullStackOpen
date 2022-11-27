8ace8f8526f156e7b426b175a9ef364a70fbcb58
jleesys949 <jlee.post24@gmail.com>
2022-11-26 16:46:45 -0800

    Add env vars

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,6 +1,7 @@
 // holds configs for server, PORT & MONGO URI
+const dotenv = require('dotenv').config();
 
 const PORT = 3001;
-const MONGODB_URI = `mongodb+srv://advent:r%401d3r@cluster0.j3mn3gt.mongodb.net/?retryWrites=true&w=majority`;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 module.exports =  { PORT, MONGODB_URI };
