ee6cac4d681db7fe2db325fe6511b5417cc47d5a
jleesys949 <jlee.post24@gmail.com>
2023-01-14 17:10:02 -0800

    Blogs sorted by likes at load

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -22,10 +22,15 @@ const App = () => {
   const blogFormRef = useRef();
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs(blogs)
-    )
+    blogService.getAll()
+      .then(blogs => {
+        blogs.sort((a, b) => {
+          return b.likes - a.likes;
+        });
+        setBlogs(blogs);
+      })
   }, [])
+
   useEffect(() => {
     try {
       const loggedUser = localStorage.getItem('currentUser');
