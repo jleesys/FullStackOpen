b4a35e09c614238ae161ad1a1e6a3420e6a8fa00
jleesys949 <jlee.post24@gmail.com>
2023-03-19 15:39:10 -0700

    Add cors

--- scratchpaper/quickrestpractice1.backend/index.js
+++ scratchpaper/quickrestpractice1.backend/index.js
@@ -1,7 +1,9 @@
 const { json } = require('body-parser');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 app.use(express.json());
+app.use(cors());
 const expensesRouter = require('./controllers/expenses');
 
 // app.use(express.static('./'));
