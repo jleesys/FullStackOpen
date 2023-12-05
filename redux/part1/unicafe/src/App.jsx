f7d9ee62aa2eddb431a051cc0d54968db3f98403
jleesys949 <jlee.post24@gmail.com>
2023-12-04 21:46:44 -0800

    Create states; button comp

--- redux/part1/unicafe/src/App.jsx
+++ redux/part1/unicafe/src/App.jsx
@@ -1,16 +1,32 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  //  const onClick = (event) => {
+  //    setGood(good + 1);
+  //    console.log(`good is at ${good}`)
+  //  }
+
+
+  const Button = function ({ text, onClick }) {
+    return (
+      <div>
+        <button onClick={onClick}>{text}</button>
+      </div>
+    )
+  }
 
   return (
     <>
       <h1>give feedback</h1>
+      <Button text='test button' />
       <button>good</button>
       <button>neutral</button>
       <button>bad</button>
       <h1>Statistics</h1>
-      good neutral bad
     </>
   )
 }
