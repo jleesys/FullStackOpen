60e353e38d6b023267055436cea78cef0bd5f400
jleesys949 <jlee.post24@gmail.com>
2023-12-31 01:56:48 -0800

    Error handling

--- redux/part2/phonebook/src/services/persons.js
+++ redux/part2/phonebook/src/services/persons.js
@@ -3,16 +3,22 @@ const baseUrl = 'http://localhost:3001/persons';
 
 const create = (person) => {
     const requestCreate = axios
-        .post(baseUrl, person);
+        .post(baseUrl, person)
+        .catch(err => {
+            console.log(err);
+        });
     return requestCreate.then(response => response.data);
 }
 
 const remove = (id) => {
     const requestDelete = axios
-        .delete(`${baseUrl}/${id}`);
+        .delete(`${baseUrl}/${id}`)
+        .catch(err => {
+            console.log(err);
+        });
     return requestDelete.then(response => response.data);
 }
 export default {
     create: create,
-    remove: remove 
+    remove: remove
 }
